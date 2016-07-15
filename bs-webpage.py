a161b9954fe6182ebbd52112a2f0a0159647e084
Kkrrish <krishmehta97@gmail.com>
2016-07-15 07:45:30 +0530

    After learning stage

--- bs-webpage.py
+++ bs-webpage.py
@@ -1,7 +1,10 @@
 from bs4 import BeautifulSoup
 import urllib
+import webbrowser
 
 r = urllib.urlopen('http://bothraclasses.com/evalution-report').read()
 soup = BeautifulSoup(r)
 
-print(soup.prettify())
+#print(soup.prettify())
+#print(type(soup))
+print(soup.find_all('script'))
