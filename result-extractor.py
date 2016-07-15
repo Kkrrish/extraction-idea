3d7e65f5258d6818927be0ec2185b0c7fdb0213d
Kkrrish <krishmehta97@gmail.com>
2016-07-15 12:10:09 +0530

    Now extracts all results and stores in CSV file

--- result-extractor.py
+++ result-extractor.py
@@ -3,64 +3,70 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import urllib
+import csv
 
 #Variables
+flag=0
+outRow=[[0,0,0,0]]
+
+#with open('results.csv', 'wb') as csvfile:
+#	results=csv.writer(csvfile,delimeter=' ', quotechar='|', quoting=csv.QUOTE_MINUMAL)
+results=csv.writer(open("results.csv","w"))
+results.writerow(["Roll Number", "Test Code", "Rank", "Marks"])
 
 #Display and driver setting
 display = Display(visible=0, size=(800, 600))
 display.start()
 driver = webdriver.Chrome()
 
+test_index=input("Which test results do you want:" )	
+print "Sure, just a minute.."
 #Getting baseurl
 driver.get('http://bothraclasses.com/evalution-report/')
 assert "Student Evaluation Report" in driver.title
 html_source=driver.page_source
 page=html_source.encode('utf-8')
-print "Original Page"
-print page
+print "Navigated to Bothra Classes Result Page"
+#print page
 
-#Giving required response
-elem=driver.find_element_by_name('rollno')
-elem.clear()
-elem.send_keys("87095")
-elem.send_keys(Keys.RETURN)
-html_source=driver.page_source
-page=html_source.encode('utf-8')
-print "Result Page"
-print page
+print "Getting the results.."
 
-#Just to check validity
-"""try:
-	assert "Invalid Roll Number" in html_source
-except:
-	flag=1
-if flag=1:
-	pass
-else :
-"""
-#Now onto BeautifulSoup!
-#"""
+for x in range(0,10):
+	for y in range(0,10):
+		rollno=87000+10*x+y
+		#Giving required response
+		elem=driver.find_element_by_name('rollno')
+		elem.clear()
+		elem.send_keys(str(rollno))
+		elem.send_keys(Keys.RETURN)
+		html_source=driver.page_source
+		page=html_source.encode('utf-8')
+		#print "Result Page"
+		#print page
 
-soup=BeautifulSoup(page)
-print(soup.prettify())
-#print(type(soup))
+		#Now onto BeautifulSoup!
+		
+		soup=BeautifulSoup(page)
+		#print(soup.prettify())
+		#print(type(soup))
 
-#Trial prints
-tr=soup.find_all('tr')
-print len(tr)
-for i in range(0,len(tr)):
-	print i
-	print tr[i]
+		tr=soup.find_all('tr')
 
-print "Now trying to find given test code"
-#test_code=6203
-test_index=input("Which test results do you want:" )	
-
-#Navigating down to the required test tr
-foundCode=soup.find_all('tr')[len(tr)-2-test_index].find_all('td')[1].contents[0]
-rank=soup.find_all('tr')[len(tr)-2-test_index].find_all('td')[4].contents[0]
-score=soup.find_all('tr')[len(tr)-2-test_index].find_all('td')[3].contents[0]
-print foundCode
-print rank
-print score
-#"""
+		#For Validity
+		if len(tr)<13:
+			flag=1
+		else:
+			#print "Now trying to find given test code"
+			#test_code=6203
+			
+			#Navigating down to the required test tr
+			foundCode=soup.find_all('tr')[len(tr)-2-test_index].find_all('td')[1].contents[0]
+			rank=soup.find_all('tr')[len(tr)-2-test_index].find_all('td')[4].contents[0]
+			score=soup.find_all('tr')[len(tr)-2-test_index].find_all('td')[3].contents[0]
+			print str(rollno)+" "+str(foundCode)+" "+str(rank)+" "+str(score)
+			#results=csv.writer(open("results.csv","w"))
+			results.writerow([int(rollno),int(foundCode),int(rank),int(score)])
+			#print foundCode
+			#print rank
+			#print score
+			#"""
